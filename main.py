f8b7d7028cb3908a41d0e1abd738bbb631d121dc
xElon-Mask <xelonmask@protonmail.com>
2022-09-02 13:49:17 +0200

    create function escalier(taille, nb)

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ t = turtle.Turtle()
 
 # faire une fonction escalier qui va prendre en compte la taille et le nombre de repetitions de marches
 
+def escalier(taille, nb):
+    for i in range(0, nb):
+        t.forward(taille)
+        t.left(90)
+        t.forward(taille)
+        t.right(90)
+    t.forward(taille)
+
 """t.forward(100)
 t.left(90)
 t.forward(50)
@@ -11,12 +19,15 @@ t.backward(100)
 """
 # exo 1 : faire un escalier avec des marches de 30 pixels de hauteur et 30 pixels de largeur, sur 5 marches
 
-for i in range(0, 5):
-    t.forward(30)
-    t.left(90)
-    t.forward(30)
-    t.right(90)
-t.forward(30)
+"""for i in range(0, 5):
+        t.forward(30)
+        t.left(90)
+        t.forward(30)
+        t.right(90)
+    t.forward(30)"""
+
+escalier(30, 5)
+
 
 turtle.done()
 

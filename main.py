e44ca6df6923e6a05b1cf4f6db8cbfb9f64dd826
xElon-Mask <xelonmask@protonmail.com>
2022-09-02 13:37:09 +0200

    create for loop to draw stairs

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import turtle
 
 t = turtle.Turtle()
 
-t.forward(100)
+# faire une fonction escalier qui va prendre en compte la taille et le nombre de repetitions de marches
+
+"""t.forward(100)
 t.left(90)
 t.forward(50)
 t.backward(100)
+"""
+# exo 1 : faire un escalier avec des marches de 30 pixels de hauteur et 30 pixels de largeur, sur 5 marches
 
+for i in range(0, 5):
+    t.forward(30)
+    t.left(90)
+    t.forward(30)
+    t.right(90)
+t.forward(30)
 
 turtle.done()
 
-# exo 1 : faire un escalier avec des marches de 30 pixels de hauteur et 30 pixels de largeur, sur 5 marches

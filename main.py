ea00f9623edb87ca361e69e4fff400f32dc74952
xElon-Mask <xelonmask@protonmail.com>
2022-09-02 13:13:11 +0200

    testing turtle's functions

---
+++ main.py
@@ -0,0 +1,13 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.backward(100)
+
+
+turtle.done()
+
+# exo 1 : faire un escalier avec des marches de 30 pixels de hauteur et 30 pixels de largeur, sur 5 marches
